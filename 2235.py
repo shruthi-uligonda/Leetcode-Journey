542b90286c6d7dbc7c60427665d1840329c6bff5
Shruthi Uligonda <Uligondashruthi@gmail.com>
2026-06-17 15:20:59 +0530

    Add a solution of 2235

---
+++ 2235.py
@@ -0,0 +1,10 @@
+# LeetCode 2235 - Add Two Integer
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return (num1 + num2)
