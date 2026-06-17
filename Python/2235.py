87235d3dd4d0c12ee4efbc0402efefa3d514dc96
Shruthi Uligonda <Uligondashruthi@gmail.com>
2026-06-17 15:23:09 +0530

    Add solution for LeetCode 2235

---
+++ Python/2235.py
@@ -0,0 +1,10 @@
+# LeetCode 2235 - Add Twp Integers
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return (num1 + num2)
