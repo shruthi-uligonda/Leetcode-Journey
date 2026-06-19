459e6f74266cd8394e1796acba771c1f426aae2e
Shruthi Uligonda <Uligondashruthi@gmail.com>
2026-06-19 15:51:23 +0530

    Pow(x,n)

---
+++ Python/0050.py
@@ -0,0 +1,10 @@
+# LeetCode 0050 - Pow(x,n)
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x ** n 
