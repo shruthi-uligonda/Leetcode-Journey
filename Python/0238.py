d8a51ee776d0f29af34b1810c50355e9e24e7e51
Shruthi Uligonda <Uligondashruthi@gmail.com>
2026-06-23 14:40:52 +0530

    Product of Array Except Self
    Implement productExceptSelf method to calculate the product of array elements except self.

---
+++ Python/0238.py
@@ -0,0 +1,20 @@
+# LeetCode 0238 - Product of Array Except Self
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        result = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            result[i] = prefix
+            prefix *= nums[i]
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+        return result
