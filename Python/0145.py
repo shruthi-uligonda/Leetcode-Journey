e4c97fd001f7ade1e9770203fb42522dd65be3e6
Shruthi Uligonda <Uligondashruthi@gmail.com>
2026-06-26 14:06:11 +0530

    Binary Tree Postorder Traversal

---
+++ Python/0145.py
@@ -0,0 +1,19 @@
+# LeetCode 0145 - Binary Tree Postorder Traversal
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        result = []
+        def postorder(curr, result):
+            if not curr:
+                return 
+            postorder(curr.left, result)
+            postorder(curr.right, result)
+            result.append(curr.val)
+        postorder(root, result)
+        return result
